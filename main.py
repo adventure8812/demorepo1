b8383a34b63c2d891854f4677ebc8b0ec6152744
adventure8812 <172941969+adventure8812@users.noreply.github.com>
2025-09-03 23:38:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
 import os
+import socket
+import struct
+import time
 
 app = Flask(__name__)
 
@@ -11,14 +14,19 @@ def health():
 def health():
    return {"status": "health2222y", "service": "ping-service"}, 200
 
-
-
-## ping service for employees
 @app.route('/ping', methods=['GET'])
 def ping():
    host = request.args.get('host', 'localhost')
-   os.system(f'ping -c 1 {host}')
-   return f"Ping sent", 200
+   try:
+       sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+       sock.settimeout(2)
+       packet = struct.pack('!BBHHH', 8, 0, 0, 1, 1)
+       sock.sendto(packet + struct.pack('!H', (~sum(struct.unpack('!4H', packet)) & 0xffff)), (socket.gethostbyname(host), 0))
+       sock.recvfrom(1024)
+       sock.close()
+       return f"Host {host} is reachable", 200
+   except:
+       return f"Host {host} is unreachable", 200
 
 if __name__ == '__main__':
    app.run(debug=False, port=5001)
