3fd7838e62cffad7c9926835fc65d962fb115691
adventure8812 <172941969+adventure8812@users.noreply.github.com>
2025-09-03 21:10:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,12 @@ def health():
    return {"status": "healthy", "service": "ping-service"}, 200
 
 
-@app.route('/health2', methods=['GET'])
-def health():
-   return {"status": "healthy2", "service": "ping-service"}, 200
-
-@app.route('/health3', methods=['GET'])
-def health():
-   return {"status": "healthy2", "service": "ping-service"}, 200
-
-
-# ## ping service for employees
-# @app.route('/ping', methods=['GET'])
-# def ping():
-#    host = request.args.get('host', 'localhost')
-#    os.system(f'ping -c 1 {host}')
-#    return f"Ping sent", 200
+## ping service for employees
+@app.route('/ping', methods=['GET'])
+def ping():
+   host = request.args.get('host', 'localhost')
+   os.system(f'ping -c 1 {host}')
+   return f"Ping sent", 200
 
 if __name__ == '__main__':
    app.run(debug=False, port=5001)
